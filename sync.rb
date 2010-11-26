674d678ed9f263cde60508213cc746eec451523b
Andrew Cholakian <andrew@andrewvc.com>
2010-11-25 21:31:39 -0800

    A few tweaks

--- sync.rb
+++ sync.rb
@@ -7,7 +7,7 @@ require './acf_invalidate.rb'
 
 puts `./generate.rb`
 puts "Syncing with S3"
-expires = (Time.now + (60*60)).httpdate
+expires = (Time.now).httpdate
 cmd = "s3cmd --acl-public --add-header 'Expires: #{expires}' --force --progress sync public/ s3://andrewvc.com/"
 puts cmd
 puts `#{cmd}`
@@ -16,6 +16,6 @@ if ARGV[0] == 'invalidate'
   conf = JSON.parse File.open('s3_config.json').read
   puts "Invalidating Cache" 
   dist = Acf.new(conf['distribution_id'], conf['access_key'], conf['secret_access_key'])
-  puts dist.invalidate('/','/js/main.js', '/images/main-graphic.png','/stylesheets/main.css').inspect
+  puts dist.invalidate('/','/index.html','/js/main.js', '/images/main-graphic.png','/stylesheets/main.css').inspect
 end
 puts "Done"
